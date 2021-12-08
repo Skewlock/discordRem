d6284595d704f8611d399c174dddfc47cdfe40e8
Skewlock <Skewlock@gmail.com>
2021-12-08 19:56:43 +0100

    updated logs

--- logs.py
+++ logs.py
@@ -1,11 +1,11 @@
 def printError(err):
-    print("\033[91m❗: "+err+"\033[00m")
+    print("\033[91m🟥: "+err+"\033[00m")
 
 def printWarning(warn):
-    print("\033[93m⚠:"+warn+"\033[00m")
+    print("\033[93m🟨:"+warn+"\033[00m")
 
 def printInfo(info):
-    print("\033[34mℹ️: "+info+"\033[00m")
+    print("\033[34m🟦: "+info+"\033[00m")
 
 def printValid(text):
-    print("\033[32m✅: "+text+"\033[00m")
+    print("\033[32m🟩: "+text+"\033[00m")
